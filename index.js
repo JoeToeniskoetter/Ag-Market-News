40381f5351625307de3743dfcd0af4ce3d68181d
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-10-22 20:57:04 -0500

    added callback to handle background notifications

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import appName from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import {AsyncStorage} from 'react-native';
 
 messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log('Message handled in the background!', remoteMessage);
+  const stored = await AsyncStorage.getItem('newlyPublishedReports');
+
+  if (!stored) {
+    console.log('NOTHING STORED');
+    const newlyPublishedReports = [];
+    newlyPublishedReports.push(remoteMessage.data.report);
+    return AsyncStorage.setItem(
+      'newlyPublishedReports',
+      JSON.stringify(newlyPublishedReports),
+    );
+  } else {
+    console.log('SAVED REPORTS', stored);
+    const newlyPublishedReports = JSON.parse(stored);
+    newlyPublishedReports.push(remoteMessage.data.report);
+    return AsyncStorage.setItem(
+      'newlyPublishedReports',
+      JSON.stringify(newlyPublishedReports),
+    );
+  }
 });
 
 AppRegistry.registerComponent('Ag Market News', () => App);
