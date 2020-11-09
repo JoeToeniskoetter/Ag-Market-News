15a18b87d468ea35278ecbc4b49e31c2a26289fd
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-11-08 22:19:22 -0600

    added subscribe and unsubscribe functions to my report provider

--- src/Providers/MyReportsProvider.tsx
+++ src/Providers/MyReportsProvider.tsx
@@ -2,7 +2,8 @@ import React, { useState, createContext, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Report } from './SearchProvider'
 import { getReportType } from '../shared/util';
-import { Alert } from 'react-native';
+import { Alert, Linking } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export type SavedReport = {
   slug_name: string;
@@ -11,17 +12,20 @@ export type SavedReport = {
 
 interface IMyReportsContext {
   reports: Report[] | [];
-  
   addReport: (rpt: Report) => void;
   removeReport: (slug_name: string) => void;
   getReports: () => void;
+  subscribeToReport: (rpt: Report) => void;
+  unsubscribeToReport: (rpt: Report) => void;
 }
 
 export const MyReportsContext = createContext<IMyReportsContext>({
   reports: [],
   addReport: async (rpt: Report) => { },
   removeReport: async (slug_name: string) => { },
-  getReports: async () => { }
+  getReports: async () => { },
+  subscribeToReport: async (rpt: Report) => { },
+  unsubscribeToReport: async (rpt: Report) => { }
 });
 
 
@@ -29,73 +33,132 @@ export const MyReportsContextProvider: React.FC<{}> = ({ children }) => {
   const [reports, setReports] = useState<Report[] | []>([]);
 
 
-  useEffect(()=>{
+  useEffect(() => {
     getReports();
   }, [])
 
+  function askToChangePermissions(){
 
-  async function addReport(rpt: Report) {
-    if (!rpt.report_url) {
-      try {
-        const res = await getReportType(rpt);
-        rpt.report_url = res.url;
-      } catch (e) {
-        return Alert.alert(e.message)
-      }
+    Alert.alert(
+      "Enable Notifications",
+      "To subscribe to reports you must enable notifications",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "Settings", onPress: () => {
+          Linking.canOpenURL('app-settings:').then(supported => {
+            if(supported){
+              Linking.openSettings()
+            }
+          })
+        }}
+      ],
+      { cancelable: true }
+    );
+
+  }
+
+  async function subscribeToReport(rpt: Report) {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    if (enabled) {
+      let newReportList: Report[] = reports.slice(0);
+      newReportList.forEach((report: Report) => {
+        if (report.slug_name === rpt.slug_name) {
+          report.subscribed = true
+        }
+      });
+      setReports(newReportList)
+      await AsyncStorage.setItem('reports', JSON.stringify(newReportList))
+      await messaging().subscribeToTopic(rpt.slug_name)
+    } else {
+      askToChangePermissions();
     }
+  }
 
-    const stored = await AsyncStorage.getItem("reports");
+async function unsubscribeToReport(rpt: Report) {
+  let newReportList: Report[] = reports.slice(0);
+  newReportList.forEach((report: Report) => {
+    if (report.slug_name === rpt.slug_name) {
+      report.subscribed = false
+    }
+  });
+  setReports(newReportList)
+  await AsyncStorage.setItem('reports', JSON.stringify(newReportList))
+  await messaging().unsubscribeFromTopic(rpt.slug_name)
+}
 
-    let rpts:Report[] = [];
 
-    if (!stored) {
-      rpts.push(rpt);
+
+async function addReport(rpt: Report) {
+  if (!rpt.report_url) {
+    try {
+      const res = await getReportType(rpt);
+      rpt.report_url = res.url;
+    } catch (e) {
+      return Alert.alert(e.message)
+    }
+  }
+
+  const stored = await AsyncStorage.getItem("reports");
+
+  let rpts: Report[] = [];
+
+  if (!stored) {
+    rpts.push(rpt);
+    await AsyncStorage.setItem("reports", JSON.stringify(rpts))
+  } else {
+
+    rpts = await JSON.parse(stored as any);
+
+    if (!rpts.some(x => x.slug_name === rpt.slug_name)) {
+      rpts.push(rpt)
       await AsyncStorage.setItem("reports", JSON.stringify(rpts))
     } else {
-
-      rpts = await JSON.parse(stored as any);
-
-      if (!rpts.some(x => x.slug_name === rpt.slug_name)) {
-        rpts.push(rpt)
-        await AsyncStorage.setItem("reports", JSON.stringify(rpts))
-      } else {
-        return;
-      }
+      return;
     }
+  }
+  setReports(rpts)
+}
+
+async function removeReport(reportId: string) {
+  let stored = await AsyncStorage.getItem("reports");
+  let storedToJson: Report[] = await JSON.parse(stored as any);
+
+  const filtered = storedToJson.filter((x) => {
+    return x.slug_name !== reportId;
+  });
+
+  await AsyncStorage.setItem('reports', JSON.stringify(filtered));
+
+  setReports(filtered);
+}
+
+async function getReports() {
+  const reports = await AsyncStorage.getItem("reports");
+  if (!reports) {
+    return []
+  } else {
+    let rpts = JSON.parse(reports);
     setReports(rpts)
   }
+}
 
-  async function removeReport(reportId: string) {
-    let stored = await AsyncStorage.getItem("reports");
-    let storedToJson: Report[] = await JSON.parse(stored as any);
-
-    const filtered = storedToJson.filter((x) => {
-      return x.slug_name !== reportId;
-    });
-
-    await AsyncStorage.setItem('reports', JSON.stringify(filtered));
-
-    setReports(filtered);
-  }
-
-  async function getReports() {
-    const reports = await AsyncStorage.getItem("reports");
-    if (!reports) {
-      return []
-    } else {
-      let rpts = JSON.parse(reports);
-      setReports(rpts)
-    }
-  }
-
-  return (
-    <MyReportsContext.Provider value={{
-      reports,
-      addReport,
-      removeReport,
-      getReports
-    }}>
-      {children}
-    </MyReportsContext.Provider>
-  )
+return (
+  <MyReportsContext.Provider value={{
+    reports,
+    addReport,
+    removeReport,
+    getReports,
+    subscribeToReport,
+    unsubscribeToReport
+  }}>
+    {children}
+  </MyReportsContext.Provider>
+)
 }
