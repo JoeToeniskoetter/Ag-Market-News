1b4de67341741a9359c0bad8271dd42c75663f6c
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-11-08 22:16:27 -0600

    moved messaging setup to subscription provider

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeStack } from './src/HomeStack/HomeStack';
@@ -10,39 +10,31 @@ import SimpleIcons from 'react-native-vector-icons/SimpleLineIcons'
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import SplashScreen from 'react-native-splash-screen';
-import messaging from '@react-native-firebase/messaging'
-import { Alert } from 'react-native';
-
-
+import { SubscriptionProvider } from './src/Providers/SubscriptionProvider';
+import { AppState } from 'react-native';
 
 const App = () => {
-
-
-  async function setUpMessaging(){
-    const permissionResult = await messaging().requestPermission();
-  }
+  
 
   useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-    });
     FontAwesome.loadFont();
     AntDesign.loadFont();
     EvilIcons.loadFont();
     SimpleIcons.loadFont();
     Ionicons.loadFont();
     SplashScreen.hide();
-    setUpMessaging();
-    return unsubscribe;
   }, [])
+
 
   return (
     <NavigationContainer>
-      <MyReportsContextProvider>
-        <SearchProvider>
-          <HomeStack />
-        </SearchProvider>
-      </MyReportsContextProvider>
+      <SubscriptionProvider>
+        <MyReportsContextProvider>
+          <SearchProvider>
+            <HomeStack />
+          </SearchProvider>
+        </MyReportsContextProvider>
+      </SubscriptionProvider>
     </NavigationContainer>
   )
 };
