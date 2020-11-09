ead34c9e9b4952910c2be454b6c1d5e3674c2ab9
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-11-08 22:19:34 -0600

    added subscription provider

--- src/Providers/SubscriptionProvider.tsx
+++ src/Providers/SubscriptionProvider.tsx
@@ -1,72 +1,103 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, {createContext, useEffect, useState} from 'react';
+import React, {useState, createContext, useEffect, useRef} from 'react';
 import { Report } from './SearchProvider';
 import messaging from '@react-native-firebase/messaging';
+import { AppState, AppStateStatic, AppStateStatus } from 'react-native';
 
 interface ISubscriptionContext{
-  subscribe: (rpt:Report) => Promise<void>;
-  unsubscribe: (slg:string) => Promise<void>;
-  subscriptions: Report[] | []
+  newReports: Report[] | [];
+  newReportViewed: (rpt:Report) => void;
+  newReportAvailable: (rpt:Report) => void;
 }
 
-const SubscriptionContext = createContext<ISubscriptionContext>({
-  subscribe: async () => {},
-  unsubscribe: async () => {},
-  subscriptions: []
+export const SubscriptionContext = createContext<ISubscriptionContext>({
+  newReports: [],
+  newReportViewed: async (rpt:Report) => {},
+  newReportAvailable: async (rpt:Report) => {}
 });
 
-
 export const SubscriptionProvider:React.FC<{}> = ({children}) => {
-const [storedSubscriptions, setStoredSubscriptions] = useState<Report[] | []>([]);
+  const [newReports, setNewReports] = useState<Report[] | []>([]);
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
 
-  async function loadSubscriptions(){
-    const storedSubscriptions  = await AsyncStorage.getItem("subscriptions");
-    if(!storedSubscriptions){
-      let subs:Report[] = [];
-      setStoredSubscriptions([]);
-      await AsyncStorage.setItem('subscriptions', JSON.stringify(subs));
-    }else{
-      setStoredSubscriptions(JSON.parse(storedSubscriptions));
+  useEffect(() => {
+    AppState.addEventListener("change", _handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener("change", _handleAppStateChange);
+    };
+  }, []);
+
+  const _handleAppStateChange = (nextAppState: AppStateStatus) => {
+    if (
+      appState.current.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      console.log("app was backgrounded. Updating subscriptions")
+      setupSubscriptionProvider();
     }
-  }
 
-  async function subscribe(rpt:Report){
-    const storedSubscriptions = await AsyncStorage.getItem('subscriptions');
-    const storedToJson:Report[] = await JSON.parse(storedSubscriptions as any);
-    if(storedToJson.some(r => r.slug_name == rpt.slug_name)){
+    appState.current = nextAppState;
+    setAppStateVisible(appState.current);
+  };
+
+  async function newReportAvailable(rpt:Report){
+    const subs = await AsyncStorage.getItem('subscriptions');  
+    const subsToJson:Report[] = await JSON.parse(subs as any);
+
+    if(subsToJson.some(r => r.slug_name === rpt.slug_name)){
       return
-    } else {
-      storedToJson.push(rpt);
-      messaging().subscribeToTopic(rpt.slug_name);
-      await AsyncStorage.setItem('subscriptions', JSON.stringify(storedToJson));
-      setStoredSubscriptions(storedToJson);
     }
+    subsToJson.push(rpt);
+    await AsyncStorage.setItem('subscriptions', JSON.stringify(subsToJson));
+    setNewReports(subsToJson);
   }
 
-  async function unsubscribe(slg:string){
-    const storedSubscriptions = await AsyncStorage.getItem('subscriptions');
-    const storedToJson:Report[] = await JSON.parse(storedSubscriptions as any);
-    if(storedToJson.some(r => r.slug_name == slg)){
-      storedToJson.filter(r => r.slug_name !== slg);
-      messaging().unsubscribeFromTopic(slg);
-      await AsyncStorage.setItem('subscriptions', JSON.stringify(storedToJson));
-      setStoredSubscriptions(storedToJson);
+  async function newReportViewed(rpt:Report){
+    const subs = await AsyncStorage.getItem('subscriptions');  
+    const subsToJson:Report[] = await JSON.parse(subs as any);
+
+    const newList = subsToJson.filter(r => r.slug_name !== rpt.slug_name);
+    await AsyncStorage.setItem('subscriptions', JSON.stringify(newList))
+    setNewReports(newList)
+
+  }
+
+  async function setupSubscriptionProvider() {
+    messaging().requestPermission();
+    let subs = await AsyncStorage.getItem('subscriptions')
+    
+    if(!subs){
+      await AsyncStorage.setItem('subscriptions', JSON.stringify([]))
+      await setNewReports([])
+    }else{
+      const subsToJson:Report[] = await JSON.parse(subs)
+      setNewReports(subsToJson);
     }
   }
 
   useEffect(()=>{
-    loadSubscriptions();
+    setupSubscriptionProvider()
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      if(!remoteMessage.data ||!remoteMessage.data.report){
+        return
+      }
+      newReportAvailable(JSON.parse(remoteMessage.data.report))
+    });
+    return unsubscribe;
   },[])
 
-return (
-  <SubscriptionContext.Provider
-  value={{
-    subscribe,
-    unsubscribe,
-    subscriptions: []
-  }}
-  >
-    {children}
-  </SubscriptionContext.Provider>
-)
+  
+
+
+  return (
+    <SubscriptionContext.Provider value={{
+      newReports,
+      newReportAvailable,
+      newReportViewed,
+    }}>
+      {children}
+    </SubscriptionContext.Provider>
+  )
 }
