6ec703f203e31f5b2835e617c3a6e06d6f7406a7
JoeToeniskotter <jtoeniskoetter.jt@gmail.com>
2020-11-08 22:17:55 -0600

    added swipe to favorite in report name search

--- src/HomeStack/SearchStack/Screens/ReportNameSearch.tsx
+++ src/HomeStack/SearchStack/Screens/ReportNameSearch.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { View, ActivityIndicator, Platform, FlatList } from 'react-native';
+import { View, ActivityIndicator, Platform, FlatList, Alert } from 'react-native';
 import { Text } from 'react-native-elements';
 
 import { SearchContext, Report } from '../../../Providers/SearchProvider';
@@ -7,20 +7,33 @@ import { SearchBar, ListItem } from 'react-native-elements';
 import { SearchNavProps } from '../SearchStackParams';
 import { Center } from './components/Center';
 import { NoResults } from './components/NoResults';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { MyReportsContext } from '../../../Providers/MyReportsProvider';
 
 interface ReportSearchProps { }
 
 export function ReportSearchScreen({ navigation, route }: SearchNavProps<"Reports">) {
   const { reportsForSearch, getReportsForSearch, loading } = useContext(SearchContext);
+  const { addReport } = useContext(MyReportsContext);
   const [searchText, setSearchText] = useState<string>('');
   const [filteredReports, setFilteredReports] = useState<Report[] | null>();;
-
+  const row: Array<any> = [];
 
   useEffect(() => {
     if (!reportsForSearch) {
       getReportsForSearch()
     }
   }, []);
+
+  const RenderLeftActions: React.FC<{}> = () => {
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', padding: 20, backgroundColor: '#39bd28' }}>
+        <FontAwesome name="star" size={24} color={"black"} />
+        <Text>Favorite</Text>
+      </View>
+    )
+  }
 
   const updateList = (term: string) => {
     setSearchText(term);
@@ -61,16 +74,29 @@ export function ReportSearchScreen({ navigation, route }: SearchNavProps<"Report
         <FlatList
           keyExtractor={(item: any) => item.slug_name}
           data={filteredReports ? filteredReports : reportsForSearch}
-          renderItem={({ item }) => <ListItem bottomDivider
-            onPress={() => {
-              navigation.navigate("PDFView", { report: item })
+          renderItem={({ item, index }) => <Swipeable
+            ref={ref => row[index] = ref}
+            renderLeftActions={() => <RenderLeftActions />}
+            onSwipeableOpen={async () => {
+              row[index].close()
+              await addReport(item)
+              await Alert.alert('Saved to Favorites')
             }}
           >
-            <ListItem.Content>
-              <ListItem.Title>{item.report_title}</ListItem.Title>
-            </ListItem.Content>
-            <ListItem.Chevron />
-          </ListItem>
+            <ListItem bottomDivider
+              onPress={() => {
+                navigation.navigate("PDFView", { report: item })
+              }}
+            >
+              <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: 'bold' }}>
+                  {item.report_title}
+                </ListItem.Title>
+                <ListItem.Subtitle>{`Report ID: ${item.slug_name}`}</ListItem.Subtitle>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+          </Swipeable>
           }
         />
       }
